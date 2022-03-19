1ba397e8a1bdf44a8cdca6efaecbd5a8833699e5
asur <asur@192.168.1.9>
2022-03-19 23:38:44 +0530

    ToDo with actual API

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -39,8 +39,6 @@ async function loadtodo(){
 
 }
 
-var totalCount = document.getElementsByClassName("paragraph").length;
-
 function handleChange(){
     if(document.getElementById("option").value === "Complete"){
         document.getElementById("complete_todos").style.display="block";
@@ -51,25 +49,4 @@ function handleChange(){
     }
     
 }
-
-// function addTodo(){
-    
-//     var inputValue = document.getElementById("todo_input").value;
-
-//     var newToDoEle = document.createElement("p")
-
-//     newToDoEle.setAttribute("key",totalCount);
-//     newToDoEle.setAttribute("class","paragraph");
-//     newToDoEle.innerText = inputValue;
-
-//     newToDoEle.onclick = function () {
-//         this.parentElement.removeChild(this);
-//     };    
-
-//     var getTodo = document.getElementsByClassName("todos");
-    
-//     getTodo[0].appendChild(newToDoEle);
-//     totalCount+=1;
-    
-// }
 
